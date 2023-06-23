ddb5bbc658b803a5189e5626d4fc403ab84c34c8
RochelManatsoa <nirinarochelmiandrisoa@gmail.com>
2023-06-23 08:07:57 +0300

    1.0.1 without default mobile image

---
+++ wordpress/wp-content/plugins/contacter-brussels/contacter-brussels.php
@@ -0,0 +1,83 @@
+<?php
+/*
+Plugin Name: Contacter Brussels
+Description: Plugin qui affiche deux images flottante sur la version mobile du site (requiert les plugins : myStickyMenu, ACF). Ajoute le framework CSS twitter Bootstrap. Corrige les liens externes et mail générant un 404 error et ajoute des balises alt sur tout les image. Ce plugin affiche egalement des polices de google fonts. Ajoute l'attribut rel="canonical" pour les contenus dupliqués.
+Version: 1.0.1
+Author: Nirina Rochel
+Author URI: https://welovedevs.com/app/fr/developer/rochel-la-ou-se-trouve-une-volonte-il-existe-un-chemin
+*/
+
+/* Prevent direct access */
+defined('ABSPATH') or die("You can't access this file directly.");
+define('NRD_PATH', plugin_dir_path(__FILE__));
+define('SITE_NAME', "contacter.brussels");
+define('SITE_NUMBER', "090488503");
+
+require_once(NRD_PATH . "/inc/functions.php");
+
+add_filter('the_content', 'popup_after_title_in_mobile');
+
+function popup_after_title_in_mobile( $content ) {
+
+    if (is_single() && !empty($GLOBALS['post'])) {
+
+        // add popup if single
+        if ($GLOBALS['post']->ID == get_the_ID()) {
+
+            $custom_content = '';
+            $second_featured_image = '';
+            $third_featured_image = '';
+            $activer_image_mobile_en_haut = '';
+            $activer_image_mobile_en_bas = '';
+            $number_click_to_call = SITE_NUMBER;
+
+            if (metadata_exists('post', get_the_ID(), 'second_featured_image') && get_post_meta(get_the_ID(), 'second_featured_image', true) !== "") {
+                $second_featured_image = wp_get_attachment_image(get_post_meta(get_the_ID(), 'second_featured_image', true), 'full');
+            } 
+
+            if (metadata_exists('post', get_the_ID(), 'third_featured_image') && get_post_meta(get_the_ID(), 'third_featured_image', true) !== "") {
+                $third_featured_image = wp_get_attachment_image(get_post_meta(get_the_ID(), 'third_featured_image', true), 'full');
+            }
+
+            if (metadata_exists('post', get_the_ID(), 'number_click_to_call') && get_post_meta(get_the_ID(), 'number_click_to_call', true) === "1") {
+                $number_click_to_call = get_post_meta(get_the_ID(), 'number_click_to_call', true);
+            } 
+
+            if (metadata_exists('post', get_the_ID(), 'activer_image_mobile_en_haut') && get_post_meta(get_the_ID(), 'activer_image_mobile_en_haut', true) === "1") {
+                $activer_image_mobile_en_haut = get_post_meta(get_the_ID(), 'activer_image_mobile_en_haut', true);
+            }
+
+            if($second_featured_image !== "" && $activer_image_mobile_en_haut === "1"){
+                $custom_content .= '<div class="container-fluid Mobile_W d-block d-sm-none text-center align-center py-3 bg-white shadow">';
+                $custom_content .= '<div class="textwidget-slide">';
+                $custom_content .= '<figure class="wp-block-image">';
+                $custom_content .= '<a href="tel:'.$number_click_to_call.'">';
+                $custom_content .= $second_featured_image;
+                $custom_content .= '</a>';
+                $custom_content .= '</figure>';
+                $custom_content .= '</div>';
+                $custom_content .= '</div>';
+            }
+
+            if (metadata_exists('post', get_the_ID(), 'activer_image_mobile_en_bas') && get_post_meta(get_the_ID(), 'activer_image_mobile_en_bas', true) !== "") {
+                $activer_image_mobile_en_bas = get_post_meta(get_the_ID(), 'activer_image_mobile_en_bas', true);
+            }
+
+            if($third_featured_image !== "" && $activer_image_mobile_en_bas === "1"){
+                $custom_content .= '<div class="container-fluid fixed-bottom d-block d-sm-none text-center align-center ">';
+                $custom_content .= '<figure>';
+                $custom_content .= '<a href="tel:'.$number_click_to_call.'">';
+                $custom_content .= $third_featured_image;
+                $custom_content .= '</a>';
+                $custom_content .= '</figure>';
+                $custom_content .= '</div>';
+            }
+
+            $custom_content .= $content;
+
+            return $custom_content;
+        }
+    }
+
+    return $content;
+}
