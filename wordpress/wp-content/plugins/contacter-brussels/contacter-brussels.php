d8c8405a9f4ea4ba3f8da96d5e0f37da2fbd17b1
RochelManatsoa <nirinarochelmiandrisoa@gmail.com>
2023-06-28 21:30:46 +0300

    duplicate contacter-brussels for serviceclient-canada

--- wordpress/wp-content/plugins/contacter-brussels/contacter-brussels.php
+++ wordpress/wp-content/plugins/contacter-brussels/contacter-brussels.php
@@ -1,8 +1,8 @@
 <?php
 /*
-Plugin Name: Contacter Brussels
+Plugin Name: Service Client Canada
 Description: Plugin qui affiche deux images flottante sur la version mobile du site (requiert les plugins : myStickyMenu, ACF). Ajoute le framework CSS twitter Bootstrap. Corrige les liens externes et mail générant un 404 error et ajoute des balises alt sur tout les image. Ce plugin affiche egalement des polices de google fonts. Ajoute l'attribut rel="canonical" pour les contenus dupliqués.
-Version: 1.0.1
+Version: 1.0.2
 Author: Nirina Rochel
 Author URI: https://welovedevs.com/app/fr/developer/rochel-la-ou-se-trouve-une-volonte-il-existe-un-chemin
 */
