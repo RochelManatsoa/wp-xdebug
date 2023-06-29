7a607b3f2ee684835f02e0047a3d3bda8b2212fc
RochelManatsoa <nirinarochelmiandrisoa@gmail.com>
2023-06-29 08:12:34 +0300

    update number & visuel

--- wordpress/wp-content/plugins/gare-de-france/gare-de-france.php
+++ wordpress/wp-content/plugins/gare-de-france/gare-de-france.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Gare de France
 Description: Plugin qui ajoute deux images flottante sur la version mobile du site (requiert les plugins: myStickyMenu, Advanced Custom Fiels). Ajoute le framework CSS twitter Bootstrap. Corrige les liens externes et mail générant un 404 error et ajoute des balises alt sur tout les image. Ce plugin affiche egalement des polices de google fonts. Ajoute l'attribut rel="canonical" pour les contenus dupliqués.
-Version: 2.2.4
+Version: 2.2.5
 Author: Nirina Rochel
 Author URI: https://rochel-nirina.welovedevs.com
 */
@@ -10,6 +10,8 @@ Author URI: https://rochel-nirina.welovedevs.com
 /* Prevent direct access */
 defined('ABSPATH') or die("You can't access this file directly.");
 define('NRD_PATH', plugin_dir_path(__FILE__));
+define('SITE_NAME', "garedefrance.fr");
+define('SITE_NUMBER', "0890214950");
 
 require_once(NRD_PATH . "/inc/functions.php");
 require_once(NRD_PATH . "/inc/simple_html_dom.php");
@@ -24,24 +26,12 @@ function popup_after_title_in_mobile( $content ) {
     if (is_array($link) || is_object($link)){
 
         foreach ($link as $value) {
-
-            // check if email
-            if(strpos($value->href, '@')){
-                if(substr($value->href, 0, 7) !== "mailto:"){
-                    $value->href = 'mailto:'.$value->href;
-                }
-            }elseif(strpos($value->href, 'tel:') === 0){
-                $value->href = 'tel:0890211805';
-            }else{
-
-                // Check https
-                if(filter_var($value->href, FILTER_VALIDATE_URL) === FALSE){
-                    $value->href = '#';
-                }elseif(substr($value->href, 0, 3) === "www"){
-                    $value->href = 'https://'.$value->href;
-                }elseif(substr($value->href, 0, 5) === "http:"){
-                    $value->href = substr($value->href, 0, 4).'s'.substr($value->href, 4);
-                }
+            if(substr($value->href, 0, 3) === "www"){
+                $value->href = 'https://'.$value->href;
+            }elseif(substr($value->href, 0, 5) === "http:"){
+                $value->href = substr($value->href, 0, 4).'s'.substr($value->href, 4);
+            }elseif(strpos($value->href, 'tel:0890211805') === 0){
+                $value->href = 'tel:'.SITE_NUMBER;
             }
             // echo '<pre>'; 
             // echo $value->href;
@@ -53,10 +43,30 @@ function popup_after_title_in_mobile( $content ) {
     if (is_array($img) || is_object($img)){
 
         foreach ($img as $value) {
-
+            if($value->src === "https://garedefrance.fr/wp-content/uploads/2023/03/VISUEL-GARE-DE-FRANCE.jpg"){
+				$value->src = "https://garedefrance.fr/wp-content/uploads/2023/06/NOUVEAU-VISUEL-0890214950.jpg";
+				if(isset($value->srcset)){
+					$value->setAttribute('srcset', str_replace('https://garedefrance.fr/wp-content/uploads/2023/03/VISUEL-GARE-DE-FRANCE', 'https://garedefrance.fr/wp-content/uploads/2023/06/NOUVEAU-VISUEL-0890214950', $value->srcset));
+				}
+			}elseif($value->src === "https://garedefrance.fr/wp-content/uploads/2023/03/GARE-DE-FRANCE-IMAGE-BLOG-BUREAU.jpg"){
+				$value->src = "https://garedefrance.fr/wp-content/uploads/2023/06/NOUVEAU-VISUEL-0890214950.jpg";
+				if(isset($value->srcset)){
+					$value->setAttribute('srcset', str_replace('https://garedefrance.fr/wp-content/uploads/2023/03/GARE-DE-FRANCE-IMAGE-BLOG-BUREAU', 'https://garedefrance.fr/wp-content/uploads/2023/06/NOUVEAU-VISUEL-0890214950', $value->srcset));
+				}
+			}elseif($value->src === "https://garedefrance.fr/wp-content/uploads/2023/03/VISUEL-GARE-DE-FRANCE-2.jpg"){
+				$value->src = "https://garedefrance.fr/wp-content/uploads/2023/06/NOUVEAU-VISUEL-0890214950.jpg";
+				if(isset($value->srcset)){
+					$value->setAttribute('srcset', str_replace('https://garedefrance.fr/wp-content/uploads/2023/03/VISUEL-GARE-DE-FRANCE-2', 'https://garedefrance.fr/wp-content/uploads/2023/06/NOUVEAU-VISUEL-0890214950', $value->srcset));
+				}
+			}elseif($value->src === "https://garedefrance.fr/wp-content/uploads/2023/03/VISUEL-GARE-DE-FRANCE-1.jpg"){
+				$value->src = "https://garedefrance.fr/wp-content/uploads/2023/06/NOUVEAU-VISUEL-0890214950.jpg";
+				if(isset($value->srcset)){
+					$value->setAttribute('srcset', str_replace('https://garedefrance.fr/wp-content/uploads/2023/03/VISUEL-GARE-DE-FRANCE-1', 'https://garedefrance.fr/wp-content/uploads/2023/06/NOUVEAU-VISUEL-0890214950', $value->srcset));
+				}
+			}
             // check alt attr defined
             if($value->alt === null || !isset($value->alt)){
-                $value->alt = 'Image dans garedefrance.fr';
+                $value->alt = 'Image dans '.SITE_NAME;
             }
         }
         
@@ -73,38 +83,35 @@ function popup_after_title_in_mobile( $content ) {
         }
 
         // add popup if single
-        if ( $GLOBALS['post']->ID == get_the_ID() ) {
-            
+        if ( $GLOBALS['post']->ID == get_the_ID() ) { 
             $custom_content = '';
-
-
+            $second_featured_image = '';
+            $third_featured_image = '';
+            $activer_image_mobile_en_haut = '';
+            $activer_image_mobile_en_bas = '';
+            $number_click_to_call = SITE_NUMBER;
+            
             if (metadata_exists('post', get_the_ID(), 'second_featured_image') && get_post_meta(get_the_ID(), 'second_featured_image', true) !== "") {
                 $second_featured_image = wp_get_attachment_image(get_post_meta(get_the_ID(), 'second_featured_image', true), 'full');
-            } else {
-                $second_featured_image = '<img class="alignnone size-full ls-is-cached lazyloaded" src="https://garedefrance.fr/wp-content/uploads/2023/03/GARE-DE-FRANCE-IMAGE-BLOG-BUREAU.jpg" alt="call service" width="298" height="281" />';
-            }
-    
+            } 
             if (metadata_exists('post', get_the_ID(), 'third_featured_image') && get_post_meta(get_the_ID(), 'third_featured_image', true) !== "") {
                 $third_featured_image = wp_get_attachment_image(get_post_meta(get_the_ID(), 'third_featured_image', true), 'full');
-            } else {
-                $third_featured_image = '<img class="alignnone size-full ls-is-cached lazyloaded" src="https://garedefrance.fr/wp-content/uploads/2023/03/garedefrance.png" alt="cartouche" width="425" height="71"/>';
             }
-    
-    
-            if (metadata_exists('post', get_the_ID(), 'number_click_to_call') && get_post_meta(get_the_ID(), 'number_click_to_call', true) !== "") {
+            if (metadata_exists('post', get_the_ID(), 'number_click_to_call') && get_post_meta(get_the_ID(), 'number_click_to_call', true) === "1") {
                 $number_click_to_call = get_post_meta(get_the_ID(), 'number_click_to_call', true);
-            } else {
-                $number_click_to_call = '0890211805';
-            }
-
-            if (metadata_exists('post', get_the_ID(), 'activer_image_mobile_en_haut') && get_post_meta(get_the_ID(), 'activer_image_mobile_en_haut', true) !== "") {
+            } 
+            if (metadata_exists('post', get_the_ID(), 'activer_image_mobile_en_haut') && get_post_meta(get_the_ID(), 'activer_image_mobile_en_haut', true) === "1") {
                 $activer_image_mobile_en_haut = get_post_meta(get_the_ID(), 'activer_image_mobile_en_haut', true);
-            }else{
-                $activer_image_mobile_en_haut = "0";
             }
 
+            /**
+             * Update number 29/06/2023
+             */
+            $activer_image_mobile_en_haut = "1";
+            $activer_image_mobile_en_bas = "0";
+            $second_featured_image = '<img loading="lazy" width="616" height="680" src="https://garedefrance.fr/wp-content/uploads/2023/06/NOUVEAU-VISUEL-0890214950.jpg" alt="" class="wp-image-25690" srcset="https://garedefrance.fr/wp-content/uploads/2023/06/NOUVEAU-VISUEL-0890214950.jpg 616w, https://garedefrance.fr/wp-content/uploads/2023/06/NOUVEAU-VISUEL-0890214950-272x300.jpg 272w" sizes="(max-width: 616px) 100vw, 616px">';
 
-            if($activer_image_mobile_en_haut !== "1"){
+            if($second_featured_image !== "" && $activer_image_mobile_en_haut === "1"){
                 $custom_content .= '<div class="container-fluid Mobile_W d-block d-sm-none text-center align-center py-3 bg-white shadow">';
                 $custom_content .= '<div class="textwidget-slide">';
                 $custom_content .= '<figure class="wp-block-image">';
@@ -118,11 +125,9 @@ function popup_after_title_in_mobile( $content ) {
 
             if (metadata_exists('post', get_the_ID(), 'activer_image_mobile_en_bas') && get_post_meta(get_the_ID(), 'activer_image_mobile_en_bas', true) !== "") {
                 $activer_image_mobile_en_bas = get_post_meta(get_the_ID(), 'activer_image_mobile_en_bas', true);
-            }else{
-                $activer_image_mobile_en_bas = "0";
             }
 
-            if($activer_image_mobile_en_bas !== "1"){
+            if($third_featured_image !== "" && $activer_image_mobile_en_bas === "1"){
                 $custom_content .= '<div class="container-fluid fixed-bottom d-block d-sm-none text-center align-center ">';
                 $custom_content .= '<figure>';
                 $custom_content .= '<a href="tel:'.$number_click_to_call.'">';
@@ -132,11 +137,9 @@ function popup_after_title_in_mobile( $content ) {
                 $custom_content .= '</div>';
             }
 
-
             $custom_content .= $html;
 
             return $custom_content;
-
         }
 
     }
--- wordpress/wp-content/plugins/gare-de-france/inc/search-personnalized.php
+++ wordpress/wp-content/plugins/gare-de-france/inc/search-personnalized.php
@@ -61,8 +61,8 @@ get_header(); ?>
                     Nous ne trouvons pas de résultat à votre recherche dans notre base. Vous pouvez faire appel à un conseiller qui fera votre recherche manuellement et pourra vous mettre en relation au besoin.
                 </p>
                 <figure class="wp-block-image size-full">
-                    <a href="tel:0890211805">
-                        <img width="768" height="372" src="https://garedefrance.fr/wp-content/uploads/2022/11/0890211805-par-REMMEDIA-pour-GDF.jpg" alt="Appeler le service" class="wp-image-68" srcset="https://garedefrance.fr/wp-content/uploads/2022/11/0890211805-par-REMMEDIA-pour-GDF.jpg 768w, https://garedefrance.fr/wp-content/uploads/2022/11/0890211805-par-REMMEDIA-pour-GDF-300x145.jpg 300w" sizes="(max-width: 767px) 89vw, (max-width: 1000px) 54vw, (max-width: 1071px) 543px, 580px">
+                    <a href="tel:<?= SITE_NUMBER ?>">
+                        <img width="768" height="372" src="https://garedefrance.fr/wp-content/uploads/2023/06/NOUVEAU-VISUEL-0890214950.jpg" alt="Appeler le service" class="wp-image-68" srcset="https://garedefrance.fr/wp-content/uploads/2022/11/0890211805-par-REMMEDIA-pour-GDF.jpg 768w, https://garedefrance.fr/wp-content/uploads/2022/11/0890211805-par-REMMEDIA-pour-GDF-300x145.jpg 300w" sizes="(max-width: 767px) 89vw, (max-width: 1000px) 54vw, (max-width: 1071px) 543px, 580px">
                     </a>
                 </figure>
             <?php
